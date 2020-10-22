63fddafd33f86b34fc5839a329fc526224f01749
James <jpinson@my365.bellevue.edu>
2020-10-21 22:20:14 -0500

    added attributions

--- week-1/hello-world.js
+++ week-1/hello-world.js
@@ -1,1 +1,13 @@
+/*
+
+Title: Assignment 1.4
+Author: James Pinson 
+Date: 21 October 2020
+Modified By: James Pinson
+Description: Introduction to primitive types
+
+*/
+
+// Introduction on how to run java script 
+
 console.log("Hello World, my name is James Pinson");
