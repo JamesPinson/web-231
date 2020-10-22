57a0403ea772f6f4e70f1ff704e8129065661ac7
James <jpinson@my365.bellevue.edu>
2020-10-21 22:04:40 -0500

    Finished Assignment 1.4

---
+++ week-1/Pinson-Assignment 1.4 - Primitive Types.js
@@ -0,0 +1,42 @@
+/*
+
+Title: Assignment 1.4
+Author: Professor Krasso
+Date: 21 October 2020
+Modified By: James Pinson
+Description: Introduction to primitive types
+
+*/
+
+// start program
+
+//This assignment focuses on primitive types.
+
+/*
+Expected output:
+
+First name
+Last name
+Age
+Street
+City
+State
+Zip Code
+*/
+
+var myFirstName= "James";
+var myLastName= "Pinson";
+var myAge= "26";
+var myStreet= "241 Maple St.";
+var myCity= "Nashville";
+var myZipCode= "37852";
+
+// output
+console.log(myFirstName);
+console.log(myLastName);
+console.log(myAge);
+console.log(myStreet);
+console.log(myCity);
+console.log(myZipCode);
+
+//end program
