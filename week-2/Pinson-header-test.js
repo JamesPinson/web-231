4e6690734c1df7011c7cc19e66b8749346c1b69b
James <jpinson@my365.bellevue.edu>
2020-10-29 22:14:29 -0500

    adding assignment 2.2.js

---
+++ week-2/Pinson-header-test.js
@@ -0,0 +1,18 @@
+/*
+============================================
+; Title: Pinson-header-test.js
+; Author: James Pinson
+; Date: 10/29/2020
+; Description: Displays a formatted header
+;============================================
+*/
+
+/**
+* Params: firstName, lastName, assignment
+* Response: output 
+* Description: Returns a well-formatted string header
+*/
+
+const header = require('./Pinson-Header.js');
+
+console.log(header.display("James", "Pinson", "Exercise 2.2"));
