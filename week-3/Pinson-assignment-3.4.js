44531cc519446e1c096835ae85eb8d6bd7a42645
James <jpinson@my365.bellevue.edu>
2020-11-08 22:15:07 -0600

    Fixing Assignment 3.4

--- week-3/Pinson-assignment-3.4.js
+++ week-3/Pinson-assignment-3.4.js
@@ -1,30 +1,15 @@
 /*
-  // Below is what I am expecting to see when I run your program - remove this before submitting your work
-  // 50% of you will not read this and lose 25% of the awardable points.
-
-  FirstName LastName
-  Assignment 3.4
-  Today's Date
-
-  -- DO THE NUMBERS MATCH GAME --
-  6 does not match 4!
-  6 does not match 7!
-  6 does not match 4!
-  6 does not match 8!
-  6 does not match 9!
-  6 does not match 7!
-  6 does match 6!
-  6 does not match 3!
+============================================
+; Title: Pinson-assignment 3.4.js
+; Author: Richard Krasso
+; Date: November 8th 2020
+; Modified By: James Pinson
+; Description: This assignment will show I know how to create a while loop statement that matches my picked number to random numbers for 10 iterations.
+;============================================
 */
 
-/**
- * Params: n/a
- * Response: integer value
- * Description: Returns a random integer value
- * Disclaimer: Do not remove this function as this is what you will be using to generate a random number
- */
 
-//Here is our assignment header.
+//Here is the assignment header.
 const header = require('./Pinson-Header.js');
 
  //Here we assign the variables for the intro header, x, and the number we picked. 
@@ -32,7 +17,7 @@ var intro = '-- DO THE NUMBERS MATCH GAME --'
 var x = 0;
 var pick= parseInt(6);
 
-//Here is the function to generate a random number as well as our match functions. 
+//This is the function to generate a random number as well as our match functions. 
 function randomNumber() {
   return Math.floor((Math.random() * 10) + 1)
 }
@@ -52,13 +37,13 @@ function match(arg1, arg2) {
     console.log(arg1 + " does match " + arg2 + "!");
   }
 
-//Here is the output we call the assignment header
-console.log(header.display("James", "Pinson", "Exercise 3.2"));
+//This is the output which calls the assignment header
+console.log(header.display("James", "Pinson", "Assignment 3.4"));
 
-//Here we call our intro header.
+//This is the output to call our intro header.
 console.log("\r\n" + intro);
 
-//Here is our while loop function which matches the number we picked versus a random number for ten iterations. 
+//This is our while loop statement which matches the number we picked versus a random number for ten iterations. 
 while (x<10){
 index = randomNumber()
 if ( match( pick, index)){
